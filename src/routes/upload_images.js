2748c2406e29fe865eb19495fdbfb1ee0d93c03b
Hacene06 <hacene06aitaissa@outlook.fr>
2021-08-07 01:29:25 +0100

    fix issue

--- src/routes/upload_images.js
+++ src/routes/upload_images.js
@@ -5,7 +5,7 @@ import { uploadImages } from '../controllers/upload';
 
 const imageRouter = Router();
 
-imageRouter.post('/image', upload.single('image'), uploadImages())
+imageRouter.post('/image', upload.single('image'), uploadImages)
 imageRouter.post('/', (req, res)=>{
     res.status(200).json({
         status:true
