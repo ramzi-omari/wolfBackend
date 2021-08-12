8421d15623d86a5819926b53806506f2c9bae3fd
Hacene06 <hacene06aitaissa@outlook.fr>
2021-08-12 23:49:01 +0100

    fix auth message error

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,9 +1,10 @@
 import passport from 'passport';
+import { UNAUTHORIZED_ACCEESS_ML_MESSAGE } from '../config/constants';
 
 export default (req, res, next) => {
   passport.authenticate(['jwt', 'jwt_admin'], function (err, user, info) {
     if (err) return next(err);
-    if (!user) return res.status(401).json({ success: false, message: 'UNAUTHORIZED_ACCEESS_ML_MESSAGE' });
+    if (!user) return res.status(401).json({ success: false, message: UNAUTHORIZED_ACCEESS_ML_MESSAGE });
     if (user.admin) {
       req.admin = user;
     }
