29f7432fb7237a6f0ba32e43410a5999052d7bf2
Hacene06 <hacene06aitaissa@outlook.fr>
2021-08-13 23:45:08 +0100

    create conversation model

---
+++ src/models/conversation.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import { RECEIVER, SENDER } from '../config/constants';
+
+const Schema = mongoose.Schema;
+
+const ConversationSchema = new Schema({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true,
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+    },
+    messages: [
+        {
+            type: Schema.ObjectId,
+            ref: 'messages',
+            required: true,
+        }
+    ],
+    unseen: {
+        type: Boolean,
+        default: true
+    },
+    unseenBy: {
+        type: String,
+        enum: [
+            RECEIVER,
+            SENDER,
+        ],
+        default: RECEIVER,
+    }
+}, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+});
+
+const Conversation = mongoose.model('conversations', ConversationSchema);
+export default Conversation;
